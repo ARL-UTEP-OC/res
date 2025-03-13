1b3d5d34ea1e41a7a302185ac2134f8d329fcab4
RaistlinJ <raistlinJ@users.noreply.github.com>
2025-03-13 10:16:53 -0600

    Created engine shell

---
+++ SEngine.py
@@ -0,0 +1,19 @@
+from engine.Engine import Engine
+import argparse
+
+def main():
+    eng = Engine.getInstance()
+    while True:
+        try:
+            command_line = input(">> ")
+            if command_line == "exit" or command_line == "quit" or command_line == "q" or command_line == "exit()":
+                break
+            print(str(eng.execute(command_line)))
+        except SystemExit:
+            # break
+            print(f"Error: bad input command")
+        except Exception as e:
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
