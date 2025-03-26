8b3ef4099336b4d6a8f7d6e39c9957b2a29eeb43
RaistlinJ <raistlinJ@users.noreply.github.com>
2025-03-25 22:22:53 -0600

    updated Pengine

--- PEngine.py
+++ PEngine.py
@@ -4,26 +4,36 @@ from engine.Configuration.UserPool import UserPool
 import Pyro5.api
 import sys
 
-if len(sys) != 2:
+if len(sys.argv) != 3:
     print("usage: PEngine.py Host-IP Host-Port")
+    exit(-1)
 
-hostip = sys.argv[0]
-hostport = sys.argv[1]
-
-daemon = Pyro5.server.Daemon(host=hostip)         # make a Pyro daemon
-ns = Pyro5.api.locate_ns(host=hostip, port=hostport)             # find the name server
+hostip = sys.argv[1]
+hostport = sys.argv[2]
+print("HOSTIP: " + str(hostip) + " HOSTPORT: " + str(hostport))
+# make a Pyro daemon
+daemon = Pyro5.server.Daemon(host=hostip)
+# find the name server
+ns = Pyro5.api.locate_ns(host=hostip, port=int(hostport))
 
 pyroEngine = Pyro5.server.expose(Engine)
-uri = daemon.register(pyroEngine)   # register the greeting maker as a Pyro object
-ns.register("engine", uri)   # register the object with a name in the name server
+# register the greeting maker as a Pyro object
+uri = daemon.register(pyroEngine)   
+# register the object with a name in the name server
+ns.register("engine", uri)   
 
 pyroUserPool = Pyro5.server.expose(UserPool)
-uri = daemon.register(pyroUserPool)   # register the greeting maker as a Pyro object
-ns.register("userpool", uri)   # register the object with a name in the name server
+# register the greeting maker as a Pyro object
+uri = daemon.register(pyroUserPool)   
+# register the object with a name in the name server
+ns.register("userpool", uri)   
 
 pyroExperimentManage = Pyro5.server.expose(ExperimentManage)
-uri = daemon.register(pyroExperimentManage)   # register the greeting maker as a Pyro object
-ns.register("experimentmanage", uri)   # register the object with a name in the name server
+# register the greeting maker as a Pyro object
+uri = daemon.register(pyroExperimentManage)   
+# register the object with a name in the name server
+ns.register("experimentmanage", uri)   
 
 print("Ready.")
-daemon.requestLoop()                   # start the event loop of the server to wait for calls
+# start the event loop of the server to wait for calls
+daemon.requestLoop()                   
