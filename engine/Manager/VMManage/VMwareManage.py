cc286a61c03852466ac4082feffe0949ac858569
RaistlinJ <raistlinJ@users.noreply.github.com>
2025-04-03 22:45:13 -0600

    bug fix

--- engine/Manager/VMManage/VMwareManage.py
+++ engine/Manager/VMManage/VMwareManage.py
@@ -695,8 +695,6 @@ class VMwareManage(VMManage):
             vmLatestSnapUUID = "1"
 
             tmpCloneName = cloneName
-            if " " in tmpCloneName and not tmpCloneName.startswith("\"") and not tmpCloneName.endswith("\""):
-                tmpCloneName = "\"" + str(tmpCloneName) + "\""
             #Call runVMCommand
             if os.path.exists(tmpCloneName):
                 logging.warning("CLONE ALREADY EXISTS, skipping: " + str(tmpCloneName))
